de5e1508f10ff045f5569158389c53a612ca0426
Emily Davis <emilyanndavis@gmail.com>
2016-07-06 14:58:41 -0600

    it works!

--- app.js
+++ app.js
@@ -5,7 +5,24 @@
 		var zip = $('#newZipCode').val();
 
 		// get the city & weather data
-		// add a city panel to the list		
+		getCity(zip, function(cityData){
+			console.log(cityData);
+
+			getWeather(zip, function(weatherData){
+				console.log(weatherData);
+
+				// add a city panel to the list	
+				var template = $('#cityTemplate').text()
+					.replace('{{ city }}', cityData.city)
+					.replace('{{ state }}', cityData.state)
+					.replace('{{ temp }}', weatherData.temp)
+					.replace('{{ conditions }}', weatherData.conditions);
+
+				$('#cityList').append($(template));
+			});
+
+		});
+
 	});
 
 
@@ -15,7 +32,14 @@
 		var url = urlBase + zip;
 
 		// use jQuery get to get the zip code data	
-		// transform the data to camelCase proprty names	
+		// transform the data to camelCase property names	
+		$.get(url, function(response){
+			callback({
+				city: response.places[0]['place name'],
+				state: response.places[0].state
+			});
+
+		});
 
 	}
 
@@ -26,5 +50,13 @@
 		var url = urlBase + 'weather?appid=' + appId + '&units=imperial&zip=' + zip;
 
 		// use jQuery get to get the zip code weather data
+		$.get(url, function(response){
+			callback({
+				temp: Math.round(response.main.temp),
+				conditions: response.weather[0].description
+			});
+		});
+
 	}
+
 })();
